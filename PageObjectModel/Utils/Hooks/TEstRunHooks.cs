7efb5228e07cdbc15b8e4d02cd810fe63b5ffb3e
Behrang Bina <BehrangBina@hotmail.com>
2018-04-24 16:14:03 +1000

    Test Run Hooks, for events of Before and After Test Execution

--- PageObjectModel/Utils/Hooks/TEstRunHooks.cs
+++ PageObjectModel/Utils/Hooks/TEstRunHooks.cs
@@ -1,6 +1,19 @@
-﻿namespace PageObjectModel.Utils.Hooks
+﻿using System.Linq;
+using PageObjectModel.Utils.Selenium;
+using TechTalk.SpecFlow;
+
+namespace PageObjectModel.Utils.Hooks
 {
-    public class TestRunHooks
+    /// <summary>
+    /// Hooks which automation logic runs before or after each test run
+    /// </summary>
+    [Binding]
+    internal static class TestRunHooks
     {
+        [AfterTestRun]
+        internal static void AfterTestRun()
+        {
+            if (!ScenarioContext.Current.ScenarioInfo.Tags.Contains("Debug")) DriverController.Instance.StopWebDriver();
+        }
     }
 }
