af9b0ee4cd6aa26ad360fcb6474951d783eb7637
Behrang Bina <BehrangBina@hotmail.com>
2018-04-24 16:09:19 +1000

    Scenario Hooks, for events of Before and After

--- PageObjectModel/Utils/Hooks/ScenarioHooks.cs
+++ PageObjectModel/Utils/Hooks/ScenarioHooks.cs
@@ -1,6 +1,29 @@
-﻿namespace PageObjectModel.Utils.Hooks
+﻿using System.Linq;
+using PageObjectModel.Utils.Selenium;
+using TechTalk.SpecFlow;
+
+namespace PageObjectModel.Utils.Hooks
 {
-    public class ScenarioHooks
+    /// <summary>
+    /// additional execution logic -> event::Scenario
+    /// </summary>
+    [Binding]
+    internal static class ScenarioHooks
     {
+        [BeforeScenario]
+        internal static void StartWebDriver()
+        {
+            // ScenarioContext: Info About State of a scenario
+            var tags = ScenarioContext.Current.ScenarioInfo.Tags;
+            if (tags.Contains("PhantomJs")) { DriverController.Instance.StartPhantomJs();}
+            else if (tags.Contains("Chrome")) { DriverController.Instance.StartChrome();}
+            else if (tags.Contains("Debug")) { DriverController.Instance.StartChrome();}
+            else DriverController.Instance.StartChrome();
+        }
+        [AfterScenario]
+        internal static void StopWebDriver()
+        {
+            if (!ScenarioContext.Current.ScenarioInfo.Tags.Contains("Debug")) DriverController.Instance.StopWebDriver();
+        }
     }
 }
